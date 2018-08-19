8e326aba14dd214773c58f4fca9a446e402a8167
Daniel Glus <danielhglus@gmail.com>
2018-08-19 01:57:44 -0400

    Fix case where category does not exist

--- src/modules/core.js
+++ src/modules/core.js
@@ -1411,7 +1411,7 @@
 
 			AFCH.api.get( request )
 				.done( function ( data ) {
-					if ( data.query.pages ) {
+					if ( data.query.pages && !data.query.pages[ "-1" ] ) {
 						var pageKey = Object.keys( data.query.pages )[ 0 ],
 							pagesCount = data.query.pages[ pageKey ].categoryinfo.pages;
 						$( '#' + countSpanId ).text( ' (' + pagesCount + ')' );
